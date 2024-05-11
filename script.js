9ddd141432fd07be539651f0d599029fc756af89
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-05-12 01:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",function (event) {
+	event.preventDefault();
+	const promis = new Promise((resolve,reject)=>{
+		const age = document.getElementById("age").value;
+		const name = document.getElementById("name").value;
+		setTimeout(()=>{
+			if(age>18){
+			resolve("Welcome, "+name+". You can vote.")
+			}
+		else{
+			reject("Oh sorry "+name+". You aren't old enough.")
+		}
+		},4000)
+	})
+promise.then((masg)=>{
+	alert(masg);
+}).catch((msg)=>{
+	alert(msg);
+})
+})
