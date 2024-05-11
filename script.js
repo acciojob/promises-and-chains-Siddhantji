498bb10a91888467eed67320eb9b002ca6249c49
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-05-12 01:36:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-//your JS code here. If required.
-document.getElementById("btn").addEventListener("click",function (event) {
-	event.preventDefault();
-	const promis = new Promise((resolve,reject)=>{
-		const age = document.getElementById("age").value;
-		const name = document.getElementById("name").value;
-		setTimeout(()=>{
-			if(age>18){
-			resolve("Welcome, "+name+". You can vote.")
-			}
-		else{
-			reject("Oh sorry "+name+". You aren't old enough.")
-		}
-		},4000)
-	})
-promise.then((masg)=>{
-	alert(masg);
-}).catch((msg)=>{
-	alert(msg);
-})
-})
+document.getElementById("btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    if (document.getElementById("age").value == null || document.getElementById("age").value == "" || document.getElementById("name").value == null || document.getElementById("name").value == 0) {
+        alert("Please enter valid details");
+    }
+    const promise = new Promise((resolve, reject) => {
+        const age = document.getElementById("age").value;
+        const name = document.getElementById("name").value;
+        setTimeout(() => {
+            if (age > 18) {
+                resolve("Welcome, " + name + ". You can vote.")
+            } else {
+                reject("Oh sorry " + name + ". You aren't old enough.")
+            }
+        }, 4000)
+    });
+
+    promise.then((message) => {
+        alert(message);
+    }).catch((message) => {
+        alert(message);
+    });
+});
